2359aa4c0f225b1f7fc20ff45767be946bd024c5
becem-gharbi <becem.gharbi@live.com>
2024-04-06 12:53:06 +0100

    chore(deps): upgrade prisma

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,6 +7,7 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
 
   nitro: {
+    preset: 'cloudflare-pages',
     modules: [nitroCloudflareBindings],
     experimental: {
       database: true
