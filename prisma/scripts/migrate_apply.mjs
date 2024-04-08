ebfd367ced181c0b4c871770c1ac083c1c5f6cf8
becem-gharbi <becem.gharbi@live.com>
2024-04-08 11:20:45 +0100

    chore: allow override database name on `migrate:apply` script

--- prisma/scripts/migrate_apply.mjs
+++ prisma/scripts/migrate_apply.mjs
@@ -4,9 +4,18 @@ import { applyMigration, getDatabaseInfo } from './utils.mjs'
 async function start () {
   try {
     const database = getDatabaseInfo()
+
     const flags = process.argv.slice(2)
     const environment = flags.includes('--remote') ? 'remote' : 'local'
-    console.log(`⌛ Apply migrations to ${environment} database \n`)
+
+    flags.forEach((flag) => {
+      const name = flag.split('--db=')[1]
+      if (name) {
+        database.name = name
+      }
+    })
+
+    console.log(`⌛ Apply migrations to ${environment} <${database.name}> database \n`)
     await applyMigration(database.name, environment)
   } catch (err) {
     console.error(`❌ ${err}`)
